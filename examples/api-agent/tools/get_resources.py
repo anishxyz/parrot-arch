9b22613b5151e23d3bf845cabcd97ae7a533abef
anishagrawal <anishagrawal2003@gmail.com>
2024-09-23 04:14:08 -0400

    init

---
+++ examples/api-agent/tools/get_resources.py
@@ -0,0 +1,12 @@
+from src.parrot.utils import tool
+
+
+@tool
+def get_resources(state: dict):
+    """
+    This tool returns a list of the resources from the REST API.
+    """
+
+    resources = state["resources"]
+    resource_list = "\n".join(f"- {resource}" for resource in resources)
+    return f"Here are resources from the API:\n{resource_list}"
