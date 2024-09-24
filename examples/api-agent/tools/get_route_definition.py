7bb9e1f3bc0b3f7c8b2b2a7d8769252f88be76e8
anishagrawal <anishagrawal2003@gmail.com>
2024-09-24 03:10:05 -0400

    docstring

--- examples/api-agent/tools/get_route_definition.py
+++ examples/api-agent/tools/get_route_definition.py
@@ -3,6 +3,8 @@ from src.parrot.utils import tool
 
 @tool
 def get_route_definition(route: str, method: str, state: dict):
+    """Returns openapi spec for route, method"""
+
     openapi = state["openapi"]
     target = openapi["paths"][route][method.lower()]
     return target
