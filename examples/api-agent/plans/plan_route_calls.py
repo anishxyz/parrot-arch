a43fe6c3c91a951e488348cf8a9864613a98d4a0
anishagrawal <anishagrawal2003@gmail.com>
2024-09-24 21:07:58 -0400

    init agent

---
+++ examples/api-agent/plans/plan_route_calls.py
@@ -0,0 +1,9 @@
+
+
+plan_prompt = """You are an agent to help users generate resources or create synthetic data for a REST API. A user will provide you with some information on what they would like to run and you will be provided with content about the API. 
+
+
+Other notes:
+You may call multiple tools at a time.
+It is recommended to look at dependency trees for all resources you may need to create.
+"""
