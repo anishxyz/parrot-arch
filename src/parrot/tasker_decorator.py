9a3534fadb11a7faabf398868469a4a9cfc53642
anishagrawal <anishagrawal2003@gmail.com>
2024-10-04 01:53:18 -0700

    tasker

---
+++ src/parrot/tasker_decorator.py
@@ -0,0 +1,43 @@
+import contextvars
+from functools import wraps
+from typing import Any, Dict, Callable
+
+StateType = Dict[str, Any]
+
+
+class Tasker:
+    def __init__(self):
+        self._context = contextvars.ContextVar('_context', default={})
+        self._history = contextvars.ContextVar('_history', default=[])
+        self._state = contextvars.ContextVar('_state', default={})
+
+    def __call__(self, cls):
+        self._init_tasker_class(cls)
+        return cls
+
+    def _init_tasker_class(self, cls):
+        cls._context = self._context
+        cls._history = self._history
+        cls._state = self._state
+
+    def setup(self, setup_func: Callable[..., StateType]):
+        def decorator(method):
+            @wraps(method)
+            def wrapper(self, *args, **kwargs):
+                if not hasattr(self, '_state'):
+                    raise TypeError("@tasker.setup can only be used in a @tasker decorated class")
+
+                current_state = self._state.get()
+                new_state = setup_func(self)
+
+                current_state.update(new_state)
+                self._state.set(current_state)
+
+                return method(self, *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+
+
+tasker = Tasker()
