5fa5dc468d997b48d389c958bd22ed123604619d
anishagrawal <anishagrawal2003@gmail.com>
2024-10-07 20:12:09 -0700

    arch

--- examples/doc_keeper/doc_keeper.py
+++ examples/doc_keeper/doc_keeper.py
@@ -3,10 +3,43 @@ from src.parrot import tasker
 
 @tasker
 class DocKeeper:
+    """
+    Orchestrating tasker
+    """
 
     @tasker.setup
-    def setup(self, filepath, thread):
+    def setup(self, config):
         # parse file to string
         # chunk file
+
+        pass
+
+    @tasker.run
+    def run(self, filepath, thread):
+        # parse file to string
+        # chunk file
+        # summarize doc
+
+        pass
+
+
+@tasker
+class DocEditor:
+    """
+    Document editor
+    """
+
+    @tasker.setup
+    def setup(self, config):
+        # parse file to string
+        # chunk file
+
+        pass
+
+    @tasker.run
+    def run(self, chunk, instruction):
+        # parse file to string
+        # chunk file
+        # summarize doc
 
         pass
